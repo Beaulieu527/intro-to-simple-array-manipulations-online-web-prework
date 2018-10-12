1376e3d5447a7658ac892696f61b9b5c3fc70c01
beaulieudev <beaulieudev@gmail.com>
2018-10-12 01:27:08 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+using_push 
+end
+
+using_unshift
+end
+
+def using_pop(array)
+end
+
+def pop_with_args(array)
+end
+
+def using_shift(array)
+end
+
+def shift_with_args(array)
+end
+
+def using_insert(array, i)
+end
+
+def using_uniq(array)
+end
+
+def using_flatten(array)
+end
+
+def using_delete(array,string)
+end
